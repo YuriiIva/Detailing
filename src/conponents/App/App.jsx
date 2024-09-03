5ffc253848199ea4625169e30044723e8cf90514
Yurii <ivaschenko_u@ukr.net>
2024-09-03 16:42:43 +0200

    add icons react

---
+++ src/conponents/App/App.jsx
@@ -0,0 +1,25 @@
+// import Contacts from 'components/Contacts/Contacts';
+// import { Routes, Route, Navigate } from 'react-router-dom';
+
+// import Main from '../Main/Main';
+// import Sidebar from '../Sidebar/Sidebar';
+import { HiMenu } from "react-icons/hi";
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div >
+        Hallo man
+        <HiMenu />
+      {/* <Sidebar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+      <Main /> */}
+    </div>
+  );
+};
+
+export default App;
